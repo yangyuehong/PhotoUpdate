60e1fc30776a8ef7baca26b9f18e06d26750c137
Yang, Yuehong | OSPD <ts-yuehong.yang@rakuten.com>
2017-07-10 14:40:31 +0900

    Initial Commit

---
+++ HealthTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthTest
+//
+//  Created by Yang, Yuehong | OSPD on 2017/07/10.
+//  Copyright © 2017年 Yang, Yuehong | OSPD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
